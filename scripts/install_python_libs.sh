8ca4d5542c67ef378754bbfd6fe5e07033f183a0
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2023-08-12 17:09:05 +0100

    Update install_python_libs.sh

--- scripts/install_python_libs.sh
+++ scripts/install_python_libs.sh
@@ -4,4 +4,6 @@ apt-get install python3-RPi.GPIO
 apt-get install python3-flask
 apt-get install python3-pigpio
 apt-get install python3-gpiozero
-apt-get install python3-falsk-socketio
+apt-get install python3-flask-socketio
+apt-get install python3-pymongo
+apt-get install python3-bson
